276f9fde7a72581c656d1098db1decf377b1a18d
aryagami27 <94152700+aryagami27@users.noreply.github.com>
2024-09-28 20:31:43 +0530

    bento upgradded

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -8,38 +8,40 @@ function About() {
     <div>
       <Events />
       <div className="col-span-2 mt-12">
-        <h2 className="text-4xl font-bold text-white">
+        <h2 className="text-4xl font-bold text-white text-center">
           About <span className="text-[#AF66DF]">KnowHow</span>
         </h2>
       </div>
-      <div className="mt-8 flex justify-center items-center">
-        <div className="grid grid-rows-4 grid-cols-6 gap-2">
-          <div className="bg-[#24242C] rounded-2xl w-full h-full col-span-4 row-span-1 p-5">
+      <div className="w-full flex justify-center">
+        <div className="mt-16 flex max-w-[1200px] max-h-[800px] justify-center items-center">
+        <div className="grid grid-cols-6 gap-2">
+          <div className="bg-[#24242C] rounded-2xl w-full h-full col-span-4 row-span-1 p-5 aspect-[4/1]">
             <p className='font-bold text-2xl'>The Spark</p>
             <p className='text-lg mt-1'>Every idea begins as a spark—fueled by curiosity, shaped by experience, and driven by a desire to learn. KnowHow was born from that very spark, a vision to create a space where knowledge flows freely and solutions take form.</p>
           </div>
-          <div className="bg-[#24242C] rounded-2xl w-full h-full row-span-1 col-span-2 flex items-center justify-center">
+          <div className="bg-[#24242C] rounded-2xl w-full h-full row-span-1 col-span-2 flex items-center justify-center aspect-[2/1]">
             Animation Maybe
           </div>
-          <div className="bg-[#24242C] rounded-2xl w-full h-full row-span-1 col-span-2 flex justify-center items-center">Animation Maybe</div>
-          <div className="bg-[#24242C] rounded-2xl w-full h-full row-span-2 col-span-2">
+          <div className="bg-[#24242C] rounded-2xl w-full h-full row-span-1 col-span-2 flex justify-center items-center aspect-[2/1]">Animation Maybe</div>
+          <div className="bg-[#24242C] rounded-2xl w-full h-full row-span-2 col-span-2 aspect-square">
             <img src='./ai.jpg' className='w-full h-full object-cover rounded-2xl' />
           </div>
-          <div className="bg-[#24242C] rounded-2xl w-full h-full row-span-2 col-span-2 p-5">
+          <div className="bg-[#24242C] rounded-2xl w-full h-full row-span-2 col-span-2 p-5 aspect-square">
             <p className='font-bold text-2xl'>Unlock Your Potential with KnowHow</p>
             <p className='text-lg mt-1'>At KnowHow, we provide the tools, resources, and community to turn your curiosity into action. Whether you're looking to learn, share, or innovate, KnowHow is where your next big idea begins.</p>
           </div>
-          <div className="bg-[#24242C] rounded-2xl w-full h-full row-span-2 col-span-2 p-5">
+          <div className="bg-[#24242C] rounded-2xl w-full h-full row-span-2 col-span-2 p-5 aspect-square">
             <p className='font-bold text-2xl'>The Journey</p>
             <p className='text-lg mt-1'>Like a path winding through uncharted territory, we guide the seekers—the innovators, the problem solvers—on their quest for answers. With each step, new doors open, leading to discovery, growth, and mastery.</p>
           </div>
-          <div className="bg-[#24242C] rounded-2xl w-full h-full row-span-1 col-span-1">
+          <div className="bg-[#24242C] rounded-2xl w-full h-full row-span-1 col-span-1 aspect-square">
             <img src='./appdev.jpg' className='w-full h-full object-cover rounded-2xl' />
           </div>
           <div className="bg-[#24242C] rounded-2xl w-full h-full row-span-1 col-span-2 flex justify-center items-center">Animation Maybe</div>
-          <div className="bg-[#24242C] rounded-2xl w-full h-full col-span-1 row-span-1">
+          <div className="bg-[#24242C] rounded-2xl w-full h-full col-span-1 row-span-1 aspect-square">
             <img src='./appdev.jpg' className='w-full h-full object-cover rounded-2xl' />
           </div>
+        </div>
         </div>
       </div>
     </div>
