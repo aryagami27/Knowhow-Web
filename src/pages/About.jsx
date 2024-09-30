834ff2562693e41fdb862cf2f5497dcf7b116768
aryagami27 <94152700+aryagami27@users.noreply.github.com>
2024-09-30 09:54:18 +0530

    improved website

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -47,7 +47,7 @@ function About() {
           <div className="grid grid-cols-4 gap-2">
             <div className="bg-[#24242C] rounded-2xl w-full h-full col-span-4 row-span-1 p-2 aspect-[4/1] text-ellipsis">
               <p className='font-bold text-sm'>The Spark</p>
-              <p className='text-[10px] text-ellipsis'>Every idea begins as a spark—fueled by curiosity, shaped by experience, and driven by a desire to learn. KnowHow was born from that very spark, a vision to create a space where knowledge flows freely and solutions take form.</p>
+              <p className='text-xs text-ellipsis'>Every idea begins as a spark—fueled by curiosity, shaped by experience, and driven by a desire to learn. KnowHow was born from that very spark, a vision to create a space where knowledge flows freely and solutions take form.</p>
             </div>
             <div className="bg-[#24242C] rounded-2xl w-full h-full row-span-1 col-span-2 flex items-center justify-center aspect-[2/1]">
               Animation Maybe
@@ -57,7 +57,7 @@ function About() {
             </div>
             <div className="bg-[#24242C] rounded-2xl w-full h-full row-span-2 col-span-2 p-2 aspect-square text-ellipsis">
               <p className='font-bold text-sm'>The Journey</p>
-              <p className='text-[10px] text-ellipsis'>Like a path winding through uncharted territory, we guide the seekers—the innovators, the problem solvers—on their quest for answers. With each step, new doors open, leading to discovery, growth, and mastery.</p>
+              <p className='text-xs text-ellipsis'>Like a path winding through uncharted territory, we guide the seekers—the innovators, the problem solvers—on their quest for answers. With each step, new doors open, leading to discovery, growth, and mastery.</p>
             </div>
             <div className="bg-[#24242C] rounded-2xl w-full h-full row-span-1 col-span-2 flex justify-center items-center aspect-[2/1]">Animation Maybe</div>
             <div className="bg-[#24242C] rounded-2xl w-full h-full row-span-1 col-span-1 aspect-square">
@@ -68,7 +68,7 @@ function About() {
             </div>
             <div className="bg-[#24242C] rounded-2xl w-full h-full row-span-2 col-span-2 p-2 aspect-square text-ellipsis">
               <p className='font-bold text-sm'>Unlock Your Potential with KnowHow</p>
-              <p className='text-[10px] text-ellipsis'>At KnowHow, we provide the tools, resources, and community to turn your curiosity into action. Whether you're looking to learn, share, or innovate, KnowHow is where your next big idea begins.</p>
+              <p className='text-xs text-ellipsis'>At KnowHow, we provide the tools, resources, and community to turn your curiosity into action. Whether you're looking to learn, share, or innovate, KnowHow is where your next big idea begins.</p>
             </div>
             <div className="bg-[#24242C] rounded-2xl w-full h-full row-span-1 col-span-2 flex justify-center items-center">Animation Maybe</div>
           </div>
