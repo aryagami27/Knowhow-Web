2726d214dfe29e9ffdd46dae63b0233236fcbc72
aryagami27 <94152700+aryagami27@users.noreply.github.com>
2024-10-01 09:47:02 +0530

    updated enroll button

--- src/pages/Events.jsx
+++ src/pages/Events.jsx
@@ -10,7 +10,7 @@ function Events() {
             <h2 className="sm:text-xl text-lg font-bold text-white">Event Title</h2>
             <p className="text-gray-400 text-xs sm:text-base">October 10th - October 12th, 2024</p>
           </div>
-          <button className="bg-[#03A21C] text-white px-4 py-2 rounded-full hover:bg-green-500 transition-colors duration-200">
+          <button className="bg-[#03A21C] shadow-[#ffffff4f] shadow-inner text-white px-4 py-2 rounded-full hover:bg-green-500 transition-colors duration-200">
             Enroll
           </button>
         </div>
@@ -19,7 +19,7 @@ function Events() {
             <h2 className="sm:text-xl text-lg font-bold text-white">Another Event</h2>
             <p className="text-gray-400 text-xs sm:text-base ">November 15th - November 17th, 2024</p>
           </div>
-          <button className="bg-[#03A21C] text-white px-4 py-2 rounded-full hover:bg-green-500">
+          <button className="bg-[#03A21C] shadow-[#ffffff4f] shadow-inner text-white px-4 py-2 rounded-full hover:bg-green-600">
             Enroll
           </button>
         </div>
