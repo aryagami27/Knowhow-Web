3e67c92e12a024e79a1f2f3664a7532b27f04858
Durgesh-05 <durgeshkumar.d@somaiya.edu>
2024-10-10 01:03:43 +0530

    Fixes Bugs

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { collection, addDoc, getDocs } from 'firebase/firestore';
+import { collection, addDoc, getDocs, query, where } from 'firebase/firestore';
 import { db } from '../firebase';
 import { useNavigate } from 'react-router-dom';
 
