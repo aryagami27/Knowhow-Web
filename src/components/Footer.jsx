2d97ae0b6097b1b3e17b6b910ad779548125877b
Durgesh-05 <durgeshkumar.d@somaiya.edu>
2024-10-09 12:30:14 +0530

    Added Whatsapp Link in Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,15 +1,21 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faInstagram, faWhatsapp, faLinkedin,faXTwitter } from '@fortawesome/free-brands-svg-icons';
+import {
+  faInstagram,
+  faWhatsapp,
+  faLinkedin,
+  faXTwitter,
+} from '@fortawesome/free-brands-svg-icons';
 
 function Footer() {
   return (
-    <footer className="bg-[#17173A] text-white py-12">
-      <div className="container mx-auto px-4">
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
+    <footer className='bg-[#17173A] text-white py-12'>
+      <div className='container mx-auto px-4'>
+        <div className='grid grid-cols-1 md:grid-cols-3 gap-8'>
           <div>
             <img src='./logo2.webp' className='w-[300px]' />
-            <p className="text-gray-400">
-              Empowering your technical growth. Providing a platform to all tech-enthusiasts out there.
+            <p className='text-gray-400'>
+              Empowering your technical growth. Providing a platform to all
+              tech-enthusiasts out there.
             </p>
           </div>
           {/* <div>
@@ -28,36 +34,61 @@ function Footer() {
             </ul>
           </div> */}
           <div>
-            <h3 className="text-lg font-semibold mb-4">Contact Us</h3>
-            <ul className="flex flex-col gap-2">
-              <a href='mailto:knowhow@gmail.com' className='hover:underline' >knowhow@gmail.com</a>
-              <a href='tel:+919828383281'className='hover:underline'>+91 98283 83281</a>
-              <a href='https://maps.app.goo.gl/TNBA5N6hhFUP5oEp9' target='_blank' className='hover:underline'>K J Somaiya Institute of Technology, Sion</a>
+            <h3 className='text-lg font-semibold mb-4'>Contact Us</h3>
+            <ul className='flex flex-col gap-2'>
+              <a href='mailto:knowhow@gmail.com' className='hover:underline'>
+                knowhow@gmail.com
+              </a>
+              <a href='tel:+919828383281' className='hover:underline'>
+                +91 98283 83281
+              </a>
+              <a
+                href='https://maps.app.goo.gl/TNBA5N6hhFUP5oEp9'
+                target='_blank'
+                className='hover:underline'
+              >
+                K J Somaiya Institute of Technology, Sion
+              </a>
             </ul>
           </div>
           <div>
-            <h3 className="text-lg font-semibold mb-4">Follow Us</h3>
-            <div className="flex space-x-6">
-              <a href="https://www.linkedin.com/company/knowhow-kjsit/" className="hover:text-gray-400" target='_blank'>
-                <FontAwesomeIcon icon={faLinkedin} size="2x" />
+            <h3 className='text-lg font-semibold mb-4'>Follow Us</h3>
+            <div className='flex space-x-6'>
+              <a
+                href='https://www.linkedin.com/company/knowhow-kjsit/'
+                className='hover:text-gray-400'
+                target='_blank'
+              >
+                <FontAwesomeIcon icon={faLinkedin} size='2x' />
               </a>
-              <a href="https://x.com/knowhow_comm" className="hover:text-gray-400" target='_blank'>
-                <FontAwesomeIcon icon={faXTwitter} size="2x" />
+              <a
+                href='https://x.com/knowhow_comm'
+                className='hover:text-gray-400'
+                target='_blank'
+              >
+                <FontAwesomeIcon icon={faXTwitter} size='2x' />
               </a>
-              <a href="https://www.instagram.com/knowhow_tech/" className="hover:text-gray-400" target='_blank'>
-                <FontAwesomeIcon icon={faInstagram} size="2x" />
+              <a
+                href='https://www.instagram.com/knowhow_tech/'
+                className='hover:text-gray-400'
+                target='_blank'
+              >
+                <FontAwesomeIcon icon={faInstagram} size='2x' />
               </a>
             </div>
-            <div className="mt-6">
-              <h3 className="text-lg font-semibold mb-4">Join Our Community</h3>
-              <a href="https://" className="flex items-center border-2 border-green-600 text-green-600 rounded-full px-3 py-2 text-sm hover:border-white hover:text-white transition-colors duration-200 w-44">
-                <FontAwesomeIcon icon={faWhatsapp} size="lg" className="mr-2" />
+            <div className='mt-6'>
+              <h3 className='text-lg font-semibold mb-4'>Join Our Community</h3>
+              <a
+                href='https://chat.whatsapp.com/JClcP75Ou4sIEfJpm7Pm6N'
+                className='flex items-center border-2 border-green-600 text-green-600 rounded-full px-3 py-2 text-sm hover:border-white hover:text-white transition-colors duration-200 w-44'
+              >
+                <FontAwesomeIcon icon={faWhatsapp} size='lg' className='mr-2' />
                 <span className='text-xs'>Join Us on WhatsApp</span>
               </a>
             </div>
           </div>
         </div>
-        <div className="mt-8 border-t border-gray-700 pt-4 text-center text-gray-400">
+        <div className='mt-8 border-t border-gray-700 pt-4 text-center text-gray-400'>
           © {new Date().getFullYear()} KnowHow. All rights reserved.
         </div>
       </div>
