f03b52b82730155527101535bf351fa58fad390e
aryagami27 <94152700+aryagami27@users.noreply.github.com>
2024-10-28 19:36:45 +0530

    Updated after events

--- src/pages/Events.jsx
+++ src/pages/Events.jsx
@@ -19,9 +19,9 @@ function Events() {
 
             </div>
           </div>
-          <Link to='/form'>
-          <button className="bg-[#03A21C] shadow-[#ffffff4f] shadow-inner text-white px-4 py-2 rounded-full hover:bg-green-500 transition-colors duration-200">
-            Enroll
+          <Link>
+          <button className="bg-[#ffffff4f] shadow-[#ffffff4f] shadow-inner text-white px-4 py-2 rounded-full transition-colors duration-200">
+            Enrollment Closed
           </button>
           </Link>
         </div>
